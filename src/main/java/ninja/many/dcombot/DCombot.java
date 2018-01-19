907d94ff4df9b79c74bf9ab892d28264868da5c6
john janes <john@janes.co>
2018-01-18 20:14:40 -0600

    config.js

--- src/main/java/ninja/many/dcombot/DCombot.java
+++ src/main/java/ninja/many/dcombot/DCombot.java
@@ -1,16 +1,40 @@
 package ninja.many.dcombot;
 
 import com.eclipsesource.v8.*;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
 
 public class DCombot {
     public static void main(String args[]) {
         V8 runtime = V8.createV8Runtime();
+        /*
         runtime.executeVoidScript(""
                 + "var person = {};\n"
                 + "var hockeyTeam = {name : 'WolfPack'};\n"
                 + "person.first = 'Ian';\n"
                 + "person['last'] = 'Bull';\n"
-                + "person.hockeyTeam = hockeyTeam;\n");
+                + "person.hockeyTeam = hockeyTeam;\n"
+        );
+
+        V8Object person = runtime.getObject("person");
+        V8Object hockeyTeam = person.getObject("hockeyTeam");
+        System.out.println(hockeyTeam.getString("name"));
+        person.release();
+        hockeyTeam.release();
+        */
+
+        String path = System.getProperty("user.dir")+"/config/config.js";
+
+        File script = new File(path);
+
+        System.out.println(System.getProperty("user.dir"));
+        try {
+            runtime.executeScript(FileUtils.readFileToString(script));
+
+
+        } catch (java.io.IOException e) {
+            System.err.println(e.getMessage());
+        }
         // TODO: Access the person object
         runtime.release();
     }
