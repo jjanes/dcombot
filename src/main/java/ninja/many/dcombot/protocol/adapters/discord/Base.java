dad4946aef8d1eaadc83e0e705c3f0100c795aed
john janes <john@janes.co>
2018-01-23 22:47:30 -0600

    protocol interface

--- src/main/java/ninja/many/dcombot/protocol/adapters/discord/Base.java
+++
@@ -1,4 +0,0 @@
-package ninja.many.dcombot.protocol.adapters.discord;
-
-public class Base {
-}
---
+++ src/main/java/ninja/many/dcombot/protocol/adapters/discord/Discord.java
@@ -0,0 +1,10 @@
+package ninja.many.dcombot.protocol.adapters.discord;
+
+import ninja.many.dcombot.protocol.Interfaces.ProtocolInterface;
+
+public class Discord implements ProtocolInterface {
+    public Discord()
+    {
+
+    }
+}
